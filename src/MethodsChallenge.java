08389537415030cc67dd40497e2ed3b24ae44031
Umar <mahmudumar476@gmail.com>
2026-02-17 10:44:56 +0100

    latest classwork

---
+++ src/MethodsChallenge.java
@@ -0,0 +1,9 @@
+public class MethodsChallenge {
+    static void welcomeMessage(){
+        System.out.println("Welcome to Java!");
+    }
+
+    public static void main(String[] args) {
+        welcomeMessage();
+    }
+}
