9d47450fec378575f7bd8434726d362febd97981
Umar <mahmudumar476@gmail.com>
2026-02-23 12:28:51 +0100

    oop

---
+++ src/Objects.java
@@ -0,0 +1,11 @@
+public class Objects{
+    String city = "London";
+    int Population =9000000;
+    public static void main(String[] args) {
+        Objects mainn = new Objects();
+        System.out.println(mainn.city);
+        System.out.println(mainn.Population);
+
+
+    }
+}
